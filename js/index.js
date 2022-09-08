a0a8894e51fc8929943a0e96c46426856db5b0dc
JarryGHerrera <jarryheremyherrera@gmail.com>
2022-09-08 07:38:22 -0700

    Reloj terminado

--- js/index.js
+++ js/index.js
@@ -1,4 +1,5 @@
 import { alarmaG, clockG } from "./dom/reloj.js";
+import darkTheme from "./dom/tema_oscuro.js";
 
 const d= document;
 
@@ -6,6 +7,8 @@ d.addEventListener("DOMContentLoaded", (e)=>{
     clockG("#mostrar-hora", "#start-reloj", "#stop-reloj");
 alarmaG("assets/graciosos-alarma-es-tu-mujer-.mp3","#start-alarma", "#stop-alarma");
 });
+
+darkTheme(".dark-theme-btn", "dark-mode");
 
 
 
